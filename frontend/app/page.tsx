323618dd2955ab5b35c142a1aed6fdf789a96ddb
spicybigmac <hongyihenrytian@gmail.com>
2025-08-02 20:25:17 -0400

    gradient

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,22 +8,14 @@ import Monitor from './components/monitor';
 
 const HeartbeatAnimation: FC = () => (
     <div className="heartbeat-bg">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2800 300">
-            <defs>
-                <linearGradient id="fade-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" stopColor="#ff3b3b" stopOpacity="1" />
-                    <stop offset="60%" stopColor="#ff3b3b" stopOpacity="1" />
-                    <stop offset="100%" stopColor="#ff3b3b" stopOpacity="1" />
-                </linearGradient>
-            </defs>
-            <g className="heartbeat-line-group">
-                <path
-                    d="M-4000 150 L100 150 L105 130 L120 190 L130 90 L140 150 L160 150 L165 170 L170 130 L180 180 L188 140 L195 150 L4000 150"
-                    stroke="url(#fade-gradient)"
-                    strokeWidth="2"
-                    fill="none"
-                />
-            </g>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 300">
+            {/* The path is now static. The CSS mask will reveal it. */}
+            <path
+                d="M-4000 150 L100 150 L105 130 L120 190 L130 90 L140 150 L160 150 L165 170 L170 130 L180 180 L188 140 L195 150 L4000 150"
+                stroke="#ff3b3b" /* Use a solid color now */
+                strokeWidth="5"
+                fill="none"
+            />
         </svg>
     </div>
 );
